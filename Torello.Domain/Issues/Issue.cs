52f79cda127bbe743831ce423c9db032910eb8fa
Tore Aursand <toreau@gmail.com>
2023-09-07 10:35:35 +0200

    Add 'IssuePriority'.

--- Torello.Domain/Issues/Issue.cs
+++ Torello.Domain/Issues/Issue.cs
@@ -6,6 +6,7 @@ public sealed class Issue : Entity<IssueId>
 {
     public string Title { get; private set; }
     public string Description { get; private set; }
+    public IssuePriority Priority { get; private set; }
     public DateTimeOffset CreatedAt { get; private set; }
     public DateTimeOffset? UpdatedAt { get; private set; }
 
@@ -13,27 +14,39 @@ public sealed class Issue : Entity<IssueId>
         IssueId id,
         string title,
         string description,
+        IssuePriority priority,
         DateTimeOffset createdAt,
         DateTimeOffset? updatedAt
     ) : base(id)
     {
         Title = title;
         Description = description;
+        Priority = priority;
         CreatedAt = createdAt;
         UpdatedAt = updatedAt;
     }
 
     public static Issue Create(
         string title,
-        string description
+        string description,
+        IssuePriority priority
     )
     {
         return new Issue(
             IssueId.CreateUnique(),
             title,
             description,
+            priority,
             DateTimeOffset.UtcNow,
             null
         );
     }
+}
+
+public enum IssuePriority
+{
+    Low,
+    Medium,
+    High,
+    Critical
 }
