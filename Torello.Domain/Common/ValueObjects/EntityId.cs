3b581fa23d49647ff5b749baafc084df0a1e54f7
Tore Aursand <toreau@gmail.com>
2023-09-15 12:13:34 +0200

    Add ability to create a new EntityId from a Guid.

--- Torello.Domain/Common/ValueObjects/EntityId.cs
+++ Torello.Domain/Common/ValueObjects/EntityId.cs
@@ -5,10 +5,16 @@ namespace Torello.Domain.Common.ValueObjects;
 public class EntityId<TThis> : ValueOf<Guid, TThis> where TThis : EntityId<TThis>, new()
 {
     public static TThis CreateUnique() => From(Guid.NewGuid());
+
     public static TThis? Create(string value)
     {
         return Guid.TryParse(value, out var guid)
             ? From(guid)
             : null;
+    }
+
+    public static TThis? Create(Guid value)
+    {
+        return From(value);
     }
 }
