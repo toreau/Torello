c8aaf2d09208029425b71a9d93d2b3414e25c04f
Tore Aursand <toreau@gmail.com>
2023-09-19 21:56:48 +0200

    Added basic Lane functionality.

---
+++ Torello.Application/Features/Lanes/Commands/UpsertBoard.cs
@@ -0,0 +1,140 @@
+using System.Net.Mime;
+using ErrorOr;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Torello.Application.Common;
+using Torello.Application.Common.Interfaces;
+using Torello.Application.Common.Interfaces.Persistence;
+using Torello.Application.Features.Lanes.Queries;
+using Torello.Contracts;
+using Torello.Domain.Boards;
+using Torello.Domain.Common.Errors;
+using Torello.Domain.Lanes;
+
+namespace Torello.Application.Features.Lanes.Commands;
+
+public sealed record UpsertLaneRequest(
+    string Title
+)
+{
+    public UpsertLaneCommand ToCommand(LaneId? laneId)
+        => new UpsertLaneCommand(
+            Title,
+            laneId,
+            null
+        );
+
+    public UpsertLaneCommand ToCommand(BoardId? boardId)
+        => new UpsertLaneCommand(
+            Title,
+            null,
+            boardId
+        );
+}
+
+public sealed record UpsertLaneCommand(
+    string Title,
+    LaneId? LaneId,
+    BoardId? BoardId
+) : IRequest<ErrorOr<LaneResult>>;
+
+[ApiExplorerSettings(GroupName = "Boards → Lanes")]
+public sealed class UpsertLaneController : ApiController
+{
+    private readonly IMediator _mediator;
+
+    public UpsertLaneController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost("/boards/{boardId:guid}/lanes", Name = nameof(CreateLane))]
+    [Produces(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(typeof(LaneResponse), 201)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> CreateLane(Guid boardId, UpsertLaneRequest upsertLaneRequest)
+    {
+        return await UpsertLane(upsertLaneRequest.ToCommand(BoardId.Create(boardId)));
+    }
+
+    [HttpPut("/lanes/{laneId:guid}", Name = nameof(UpdateLane))]
+    [Produces(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(typeof(LaneResponse), 200)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> UpdateLane(Guid laneId, UpsertLaneRequest upsertLaneRequest)
+    {
+        return await UpsertLane(upsertLaneRequest.ToCommand(LaneId.Create(laneId)));
+    }
+
+    private async Task<IActionResult> UpsertLane(UpsertLaneCommand upsertLaneCommand)
+    {
+        var upsertLaneResult = await _mediator.Send(upsertLaneCommand);
+
+        return upsertLaneResult.Match(
+            result =>
+            {
+                var response = result.ToResponse();
+
+                return upsertLaneCommand.LaneId is null
+                    ? CreatedAtRoute(
+                        nameof(GetLaneByIdController.GetLaneById),
+                        new { boardId = response.Id },
+                        response)
+                    : Ok(response);
+            },
+            errors => Problem(errors)
+        );
+    }
+}
+
+internal sealed class UpsertLaneHandler : IRequestHandler<UpsertLaneCommand, ErrorOr<LaneResult>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IAuthService _authService;
+
+    public UpsertLaneHandler(IUnitOfWork unitOfWork, IAuthService authService)
+    {
+        _unitOfWork = unitOfWork;
+        _authService = authService;
+    }
+
+    public async Task<ErrorOr<LaneResult>> Handle(
+        UpsertLaneCommand upsertLaneCommand,
+        CancellationToken cancellationToken
+    )
+    {
+        if (await _authService.GetCurrentUserAsync() is not { } user)
+            return Errors.Users.InvalidCredentials;
+
+        Lane? lane;
+
+        // Update an existing lane?
+        if (upsertLaneCommand.LaneId is not null)
+        {
+            // Does the lane exist?
+            lane = await _unitOfWork.Lanes.GetByIdAsync(upsertLaneCommand.LaneId);
+            if (lane is null)
+                return Errors.Lanes.NotFound;
+
+            // Is the lane's owner the same as the one logged in?
+            if (lane.UserId != user.Id)
+                return Errors.Users.InvalidCredentials;
+
+            lane.Update(upsertLaneCommand.Title);
+        }
+        else
+        {
+            // Does the board exist?
+            if (await _unitOfWork.Boards.GetByIdAsync(upsertLaneCommand.BoardId!) is not { } board)
+                return Errors.Boards.NotFound;
+
+            lane = Lane.Create(upsertLaneCommand.Title);
+
+            board.AddLane(lane);
+        }
+
+        await _unitOfWork.SaveChangesAsync();
+
+        return new LaneResult(lane);
+    }
+}
---
+++ Torello.Application/Features/Lanes/Queries/GetAllLanes.cs
@@ -0,0 +1,74 @@
+using System.Net.Mime;
+using ErrorOr;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Torello.Application.Common;
+using Torello.Application.Common.Interfaces;
+using Torello.Application.Common.Interfaces.Persistence;
+using Torello.Contracts;
+using Torello.Domain.Boards;
+using Torello.Domain.Common.Errors;
+
+namespace Torello.Application.Features.Lanes.Queries;
+
+internal sealed record GetAllLanesQuery(
+    Guid BoardId
+) : IRequest<ErrorOr<LanesResult>>;
+
+[ApiExplorerSettings(GroupName = "Boards → Lanes")]
+public class GetAllLanesController : ApiController
+{
+    private readonly IMediator _mediator;
+
+    public GetAllLanesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("/boards/{boardId:guid}/lanes")]
+    [Produces(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(typeof(BoardsResponse), 200)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> GetAllLanes(Guid boardId)
+    {
+        var getAllLanesQuery = new GetAllLanesQuery(boardId);
+        var lanesResult = await _mediator.Send(getAllLanesQuery);
+
+        return lanesResult.Match(
+            result => Ok(result.ToResponse()),
+            errors => Problem(errors)
+        );
+    }
+}
+
+internal sealed class GetAllLanesHandler : IRequestHandler<GetAllLanesQuery, ErrorOr<LanesResult>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IAuthService _authService;
+
+    public GetAllLanesHandler(IUnitOfWork unitOfWork, IAuthService authService)
+    {
+        _unitOfWork = unitOfWork;
+        _authService = authService;
+    }
+
+    public async Task<ErrorOr<LanesResult>> Handle(
+        GetAllLanesQuery getAllLanesQuery,
+        CancellationToken cancellationToken
+    )
+    {
+        if (BoardId.Create(getAllLanesQuery.BoardId) is not { } boardId)
+            return Errors.EntityId.Invalid;
+
+        if (await _authService.GetCurrentUserAsync() is not { } user)
+            return Errors.Users.InvalidCredentials;
+
+        if (await _unitOfWork.Boards.GetByIdAsync(boardId) is not { } board)
+            return Errors.Boards.NotFound;
+
+        if (board.UserId != user.Id)
+            return Errors.Users.InvalidCredentials;
+
+        return new LanesResult(board.Lanes);
+    }
+}
