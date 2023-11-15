c8fd7917e8a3cfae4d8cf43eac45e1fcfa301fd1
Tore Aursand <toreau@gmail.com>
2023-11-15 12:42:20 +0100

    Convert to use primary constructor.

--- Torello.Domain/Common/Primitives/Entity.cs
+++ Torello.Domain/Common/Primitives/Entity.cs
@@ -1,17 +1,12 @@
 namespace Torello.Domain.Common.Primitives;
 
-public abstract class Entity<TEntityId> : IDomainEventProvider
+public abstract class Entity<TEntityId>(TEntityId id) : IDomainEventProvider
 {
-    public TEntityId Id { get; protected set; }
+    public TEntityId Id { get; protected set; } = id;
 
-    private readonly List<IDomainEvent> _domainEvents = new List<IDomainEvent>();
+    private readonly List<IDomainEvent> _domainEvents = new();
 
     public IReadOnlyList<IDomainEvent> DomainEvents() => _domainEvents.AsReadOnly();
-
-    protected Entity(TEntityId id)
-    {
-        Id = id;
-    }
 
     public void AddDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
     public void ClearDomainEvents() => _domainEvents.Clear();
