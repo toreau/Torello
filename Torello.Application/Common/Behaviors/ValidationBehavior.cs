4d147c2bec83fa7d1bbc0e584ae2644326b05f14
Tore Aursand <toreau@gmail.com>
2023-11-15 15:18:24 +0100

    Improved documentation.

--- Torello.Application/Common/Behaviors/ValidationBehavior.cs
+++ Torello.Application/Common/Behaviors/ValidationBehavior.cs
@@ -4,31 +4,42 @@ using MediatR;
 
 namespace Torello.Application.Common.Behaviors;
 
-public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+/// <summary>
+/// Generic ValidationBehavior that implements IPipelineBehavior for MediatR.
+/// </summary>
+/// <param name="validator"></param>
+/// <typeparam name="TRequest"></typeparam>
+/// <typeparam name="TResponse"></typeparam>
+public class ValidationBehavior<TRequest, TResponse>(IValidator<TRequest>? validator = null)
+    : IPipelineBehavior<TRequest, TResponse>
     where TRequest : IRequest<TResponse>
     where TResponse : IErrorOr
 {
-    private readonly IValidator<TRequest>? _validator;
-
-    public ValidationBehavior(IValidator<TRequest>? validator = null)
-    {
-        _validator = validator;
-    }
-
+    /// <summary>
+    /// Handle method required by IPipelineBehavior to perform validation in the pipeline.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="next"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     public async Task<TResponse> Handle(
         TRequest request,
         RequestHandlerDelegate<TResponse> next,
         CancellationToken cancellationToken
     )
     {
-        if (_validator is null)
+        // If no validator provided, proceed to the next handler.
+        if (validator is null)
             return await next();
 
-        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+        // Validate the request.
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
 
+        // If valid, proceed to the next handler.
         if (validationResult.IsValid)
             return await next();
 
+        // Validation has failed, so lets create a list of the validation errors and return them.
         var errors = validationResult.Errors
             .ConvertAll(validationFailure => Error.Validation(
                 validationFailure.PropertyName,
