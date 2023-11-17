6f8166e9c29a9087921879f1fd21f2c6d5eaddfc
Tore Aursand <toreau@gmail.com>
2023-11-17 11:59:05 +0100

    Add nice to have attributes.

--- Torello.Api/Controllers/HealthController.cs
+++ Torello.Api/Controllers/HealthController.cs
@@ -1,10 +1,13 @@
+using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Torello.Api.Controllers;
 
 public class HealthController : ControllerBase
 {
-    [HttpGet("/_health")]
+    [HttpGet("/_health", Name = nameof(GetHealth))]
+    [Produces(MediaTypeNames.Application.Json)]
+    [ProducesResponseType(typeof(IActionResult), 200)]
     public IActionResult GetHealth()
     {
         return Ok(new
