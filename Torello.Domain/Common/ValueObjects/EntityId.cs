9415c14e6b84eb8a7c591630cde6b163677b4772
Tore Aursand <toreau@gmail.com>
2023-12-30 13:41:52 +0100

    Use SequentialGuid to create - eh - sequential guids for the database indexes to love. 🤪

--- Torello.Domain/Common/ValueObjects/EntityId.cs
+++ Torello.Domain/Common/ValueObjects/EntityId.cs
@@ -1,10 +1,11 @@
+using SequentialGuid;
 using ValueOf;
 
 namespace Torello.Domain.Common.ValueObjects;
 
 public class EntityId<TThis> : ValueOf<Guid, TThis> where TThis : EntityId<TThis>, new()
 {
-    public static TThis CreateUnique() => From(Guid.NewGuid());
+    public static TThis CreateUnique() => From(SequentialGuidGenerator.Instance.NewGuid());
 
     public static TThis? Create(string value)
     {
